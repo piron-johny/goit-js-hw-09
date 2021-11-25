f037d41f49c4eec04ea3cb7eff492e5a1c771141
Piron Evgeniy <piron.johny@gmail.com>
2021-11-25 14:44:57 +0000

    task 3.1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,13 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+
 const form = document.querySelector('.form');
-const delay = document.querySelector('input[name=delay]');
+const delayInp = document.querySelector('input[name=delay]');
 const step = document.querySelector('input[name=step]');
 const amount = document.querySelector('input[name=amount]');
 const submitBtn = form.querySelector('button');
-let stepTimer = null;
-const promises = [];
 
+let stepTimer = null;
 let counterPromise = 0;
 
 form.addEventListener('submit', e => {
@@ -15,34 +17,33 @@ form.addEventListener('submit', e => {
 submitBtn.addEventListener('click', onClickSubmit);
 
 function onClickSubmit(e) {
-  console.log('amount', +amount.value);
-
   stepTimer = setInterval(() => {
     counterPromise += 1;
-    console.log(counterPromise);
+
     if (counterPromise === +amount.value) {
       clearInterval(stepTimer);
-      console.log('STOP!');
     }
-    createPromise(counterPromise, delay.value);
-    // createPromise(counterPromise, delay.value)
-    //   .then(({ position, delay }) => {
-    //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    //   })
-    //   .catch(({ position, delay }) => {
-    //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    //   });
+
+    createPromise(counterPromise, delay.value)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }, step.value);
 }
 
-
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    resolve({ position, delay });
-  } else {
-    // Reject
-    reject("Error! Error passed to reject function");
-  }
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
