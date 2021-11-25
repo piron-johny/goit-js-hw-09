4a6cef310a5dbbd15606ab6bb6c7c9bd03bdb0da
Piron Evgeniy <piron.johny@gmail.com>
2021-11-25 12:09:28 +0000

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,7 +3,7 @@ const delay = document.querySelector('input[name=delay]');
 const step = document.querySelector('input[name=step]');
 const amount = document.querySelector('input[name=amount]');
 const submitBtn = form.querySelector('button');
-const stepTimer = null;
+let stepTimer = null;
 const promises = [];
 
 let counterPromise = 0;
@@ -15,17 +15,17 @@ form.addEventListener('submit', e => {
 submitBtn.addEventListener('click', onClickSubmit);
 
 function onClickSubmit(e) {
-
-  setTimeout(() => {
-    clearInterval(stepTimer)
-  }, (step.value * amount.value));
+  console.log('amount', +amount.value);
 
   stepTimer = setInterval(() => {
+    counterPromise += 1;
     console.log(counterPromise);
-    counterPromise += 1
-    promises.push('promise' + counterPromise);
-
-    // createPromise(amount.value, delay.value)
+    if (counterPromise === +amount.value) {
+      clearInterval(stepTimer);
+      console.log('STOP!');
+    }
+    createPromise(counterPromise, delay.value);
+    // createPromise(counterPromise, delay.value)
     //   .then(({ position, delay }) => {
     //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
     //   })
@@ -33,15 +33,16 @@ function onClickSubmit(e) {
     //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     //   });
   }, step.value);
-
-  console.log(promises);
 }
+
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    resolve({ position, delay });
   } else {
     // Reject
+    reject("Error! Error passed to reject function");
   }
 }
