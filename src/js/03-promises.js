62d2f26715761479a9cf13c87fd2c91e59441bb0
Piron Evgeniy <piron.johny@gmail.com>
2021-11-26 09:30:06 +0000

    finish task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,13 +2,14 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const form = document.querySelector('.form');
-const delay = document.querySelector('input[name=delay]');
+const delayInput = document.querySelector('input[name=delay]');
 const step = document.querySelector('input[name=step]');
 const amount = document.querySelector('input[name=amount]');
 const submitBtn = form.querySelector('button');
 
 let stepTimer = null;
 let counterPromise = 0;
+let delayCount = 0;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -20,18 +21,31 @@ function onClickSubmit(e) {
   stepTimer = setInterval(() => {
     counterPromise += 1;
 
+    delayCount += +step.value
+
+    if (counterPromise === 1) {
+      delayCount = +delayInput.value;
+    }
+
     if (counterPromise === +amount.value) {
       clearInterval(stepTimer);
     }
 
-    createPromise(counterPromise, delay.value)
+    console.log('count', counterPromise);
+    console.log('del', delayCount);
+
+    createPromise(counterPromise, delayCount)
       .then(({ position, delay }) => {
+
         Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
   }, step.value);
+
+  counterPromise = 0;
+  delayCount = 0;
 }
 
 function createPromise(position, delay) {
